bd94477e7c1da37d919084693afe57e6404251c9
Bill Transue <transue@gmail.com>
2017-03-27 11:44:00 -0400

    Finish NavHelper and specs

--- lib/ui_helpers/nav_helper.rb
+++ lib/ui_helpers/nav_helper.rb
@@ -1,7 +1,7 @@
 module UIHelpers
   module NavHelper
     def nav(options={})
-      content_tag :ul, options.except(:class).merge(class: "nav #{options.delete(:class)}") do
+      content_tag :ul, class: "nav nav-#{options[:type]} #{options[:class]}" do
         yield NavBuilder.new(options, self)
       end
     end
@@ -11,12 +11,28 @@ module UIHelpers
       delegate :capture, :content_tag, :link_to, to: :template
 
       def initialize(options={}, template)
-        @template = template
+        @template = template || options
       end
 
-      def pill(ref, title, options={}, &block)
+      def pill(ref=nil, title=nil, options={}, &block)
+        title ||= ref.to_s
+        ref = title.parameterize.underscore.to_sym if title == ref
         content_tag :li, role: 'presentation', class: options[:active] ? 'active' : '' do
-          link_to title, "##{ref}", 'aria-controls' => ref, role: 'pill', 'data-toggle' => 'pill'
+          if block_given?
+            link_to "##{ref}", 'aria-controls' => ref, role: 'pill', 'data-toggle' => 'pill', &block
+          else
+            link_to title, "##{ref}", 'aria-controls' => ref, role: 'pill', 'data-toggle' => 'pill'
+          end
+        end
+      end
+
+      def tab(title=nil, options={}, &block)
+        content_tag :li, role: 'presentation', class: options[:active] ? 'active' : '' do
+          if block_given?
+            link_to '#', &block
+          else
+            link_to title, '#'
+          end
         end
       end
     end
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -4,16 +4,24 @@ module Helpers
     StringRenderer.new(string)
   end
 
-  class StringRenderer < ActionView::Template::Handlers::Erubis
-    def with(*modules)
-      object = Object.new
-      modules.each { |name| object.extend name }
+  def construct_template(*modules)
+    object = Object.new
+    modules.each { |name| object.extend name }
 
-      Capybara::Node::Simple.new result(object
-        .extend(ActionView::Helpers)
-        .extend(ActionView::Helpers::UrlHelper)
-        .extend(ActionView::Context)
-        .instance_eval { binding })
+    object
+      .extend(ActionView::Helpers)
+      .extend(ActionView::Helpers::UrlHelper)
+      .extend(ActionView::Context)
+  end
+
+  def construct_binding(*modules)
+    construct_template(*modules).instance_eval { binding }
+  end
+
+  class StringRenderer < ActionView::Template::Handlers::Erubis
+    include Helpers
+    def with(*modules)
+      Capybara::Node::Simple.new result(construct_binding(*modules))
     end
   end
 end
