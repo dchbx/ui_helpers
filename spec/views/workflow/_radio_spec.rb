1ee9ef71c039e96c3273ae07e9c72b9d2348cb0d
Mahesh Kurma <maheshruby405@gmail.com>
2017-03-31 14:19:19 -0400

    Add _radio partial for radio lines

--- spec/views/workflow/_radio_spec.rb
+++ spec/views/workflow/_radio_spec.rb
@@ -1,11 +1,19 @@
 require "rails_helper"
 
 describe 'workflow/_radio' do
+  let(:line) do
+    UIHelpers::Workflow::Line.new 'text' => 'Yo?', 'values' => ['Yo!', 'No!']
+  end
+
   before do
-    render partial: 'workflow/radio'
+    render partial: 'workflow/radio', locals: { line: line }
   end
 
   it 'renders' do
     expect(rendered).to_not be_nil
+  end
+
+  it 'has radio buttons' do
+  	expect(rendered).to have_css('input[type=radio]')
   end
 end
