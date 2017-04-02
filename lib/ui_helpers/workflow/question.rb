62d1583cbb9d2c99604a39c3513e7b9e8b3f8ba7
shaibazalisyed <shaibazali34@gmail.com>
2017-04-02 11:30:16 -0400

    dropsown and multi row

---
+++ lib/ui_helpers/workflow/question.rb
@@ -0,0 +1,24 @@
+module UIHelpers
+  module Workflow
+    class Question
+      attr_accessor :text, :type, :values
+
+      def initialize(attributes)
+        @text = attributes['text']
+        @type = attributes['type']
+        @values = attributes['values']
+      end
+
+      def to_partial_path
+        case type
+        when 'radio'
+          'workflow/radio'
+        when 'dropdown'
+          'workflow/dropdown'
+        when 'multi_row'
+          'workflow/multi_row'
+        end
+      end
+    end
+  end
+end
