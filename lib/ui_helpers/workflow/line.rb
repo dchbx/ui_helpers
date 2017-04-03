2bb34c278ead709a26bedea21844c1695045072f
shaibazalisyed <shaibazali34@gmail.com>
2017-04-03 11:33:06 -0400

    added dropdown and multirow partials

--- lib/ui_helpers/workflow/line.rb
+++ lib/ui_helpers/workflow/line.rb
@@ -1,13 +1,14 @@
 module UIHelpers
   module Workflow
     class Line
-      attr_accessor :text, :type, :values, :options
+      attr_accessor :text, :type, :values, :options, :fields
 
       def initialize(attributes)
         @text = attributes['text']
         @type = attributes['type']
         @values = attributes['values']
         @options = attributes['options'] || {}
+        @fields = attributes['fields']
       end
 
       def to_partial_path
